99c4220d973ea049cdd4c9f72b3d82d0c77cf27a
jungse8609 <jungse8609@gmail.com>
2025-10-12 16:37:26 +0900

    251011_BorderJung

--- BorderJung/Unsolved/3015_오아시스재결합.cpp
+++ BorderJung/Unsolved/3015_오아시스재결합.cpp
@@ -1,35 +1,43 @@
-// Authored by : border jung
 #include <iostream>
 #include <stack>
+#define x first
+#define y second
 using namespace std;
-#define X first
-#define Y second
 
-int main(void) {
-    ios::sync_with_stdio(0);
+int main()
+{
     cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    /*
+    Stack에 순서대로 쌓아가면서 전 사람과 키를 비교.
+    1. 만약 전 사람보다 키가 크거나 같으면?
+        전 사람을 볼 수 있으면 나도 보겠지.
+    2. 만약 전 사람보다 키가 작으면?
+        나를 볼 수 있는건 전 사람 뿐임
+    */
 
     int n; cin >> n;
+    stack<pair<int, int>> people; // 자기 키, 쌍 수
 
-    stack<pair<int, int>> S; // {height, 연속한 동일한 숫자 개수, 
-    long long ans = 0;
-    while (n--) 
+    long long result = 0;
+    while (n--)
     {
         int h; cin >> h;
         int cnt = 1;
-        
-        while (!S.empty() && S.top().X <= h) 
+        while (!people.empty() && people.top().x <= h)
         {
-            ans += S.top().Y;
-            if (S.top().X == h) 
-                cnt += S.top().Y;
-            S.pop();
+            result += people.top().y;
+            if (people.top().x == h)
+                cnt += people.top().y;
+            people.pop(); 
         }
 
-        if (!S.empty()) 
-            ans++;
+        if (!people.empty())
+            result += 1;
         
-        S.push({h, cnt});
+        people.push({h, cnt});
     }
-    cout << ans;
+
+    cout << result;
 }
