eaecf342be1eed34e4e86ded47fff48722f172d9
Lee Si Woong <silverttthin@naver.com>
2026-01-02 16:41:22 +0900

    260102_newStart

---
+++ siuuuuung/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        String[] s1 = {"i", "drink", "water"};
+        String[] s2 = {"want", "to"};
+        String[] goal = {"i", "want", "to", "drink", "water"};
+
+        ["i", "drink", "water"], ["want", "to"], ["i", "want", "to", "drink", "water"]
+        System.out.println(sol.solution(s1, s2, goal));
+    
+        
+    }
+}
+
